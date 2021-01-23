394ae888c0222435d8fae6fbebf028e47430ae1c
unclekingfish <gaoyi11bobo@hotmail.com>
2021-01-23 18:39:25 +0800

    test

---
+++ test.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int month = 8;
+//int main()
+//{
+//	
+//	
+//	///*har str = 'a';
+//	//printf("str = %c\n", str);*/  //%c表示输出一个字符
+//
+//
+//		char str[] = "a";
+//	printf("star = %s\n", str);  //%s 表示输出一个字符串
+//
+//
+//		return 0;
+//
+//} 
