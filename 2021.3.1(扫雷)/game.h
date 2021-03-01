c2c48a1dc11494d093375cc7e6f8ff379f6c3670
金鱼佬 <76930240+unclekingfish@users.noreply.github.com>
2021-03-01 22:16:31 +0800

    Delete 2021.3.1(扫雷) directory

--- 2021.3.1(扫雷)/game.h
+++
@@ -1,8 +0,0 @@
-#include<stdio.h>
-#define ROW 9
-#define COL 9
-#define ROWS ROW+2
-#define COLS COL+2
-
-char InitBoard(char board[ROWS][COLS], int row, int cols,char set);
-void DisplayBoard(char board[ROWS][COLS], int row, int col);
